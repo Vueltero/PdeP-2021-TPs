584f791ffa90ee9e14ad627a3280a01d5011c8d7
Vueltero <vueltaagustin@gmail.com>
2021-05-09 16:41:26 -0300

    punto 1

--- Funcional/Fuente de los Deseos/TP1Funcional.hs
+++ Funcional/Fuente de los Deseos/TP1Funcional.hs
@@ -28,3 +28,19 @@ cantidadSuenosPersona :: Persona -> CantidadSuenios
 cantidadSuenosPersona (_, cantidadSuenios, _, _, _) = cantidadSuenios
 
 -- Punto b: Grado de ambicion de una persona
+gradoAmbicion unaPersona
+	| felicidoniosPersona unaPersona > 100 = (felicidoniosPersona unaPersona) * (cantidadSuenosPersona unaPersona)
+	| (felicidoniosPersona unaPersona <= 100) && (felicidoniosPersona unaPersona > 50) = (edadPersona unaPersona) * (cantidadSuenosPersona unaPersona)
+	| otherwise = (cantidadSuenosPersona unaPersona) * 2
+
+-- Punto 2
+-- Punto a: Nombre largo
+nombreLargo (_, _, nombre, _, _) = length nombre > 10
+
+nombrePersona (_, _, nombre, _, _) = nombre
+nombreLargo unaPersona = length (nombrePersona unaPersona) > 10
+
+nombreLargo = (length . nombrePersona) > 10
+
+nombreLargo = (> 10) . length . nombrePersona
+
