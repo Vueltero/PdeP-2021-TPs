c85d735b6af059a454b1b34e7806b93c83c3dde7
Vueltero <vueltaagustin@gmail.com>
2021-05-09 19:07:55 -0300

    Terminado

--- Funcional/Fuente de los Deseos/TP1Funcional.hs
+++ Funcional/Fuente de los Deseos/TP1Funcional.hs
@@ -13,6 +13,7 @@ type Persona = (Edad, CantidadSuenios, Nombre, Felicidonios, Habilidades)
 
 -- Punto 1
 -- Punto a: Coeficiente de satisfaccion
+coeficienteSatisfaccion :: Persona -> Int
 coeficienteSatisfaccion unaPersona
 	| felicidoniosPersona unaPersona > 100 = (felicidoniosPersona unaPersona) * (edadPersona unaPersona)
 	| (felicidoniosPersona unaPersona <= 100) && (felicidoniosPersona unaPersona > 50) = (cantidadSuenosPersona unaPersona) * (felicidoniosPersona unaPersona)
@@ -28,6 +29,7 @@ cantidadSuenosPersona :: Persona -> CantidadSuenios
 cantidadSuenosPersona (_, cantidadSuenios, _, _, _) = cantidadSuenios
 
 -- Punto b: Grado de ambicion de una persona
+gradoAmbicion :: Persona -> Int
 gradoAmbicion unaPersona
 	| felicidoniosPersona unaPersona > 100 = (felicidoniosPersona unaPersona) * (cantidadSuenosPersona unaPersona)
 	| (felicidoniosPersona unaPersona <= 100) && (felicidoniosPersona unaPersona > 50) = (edadPersona unaPersona) * (cantidadSuenosPersona unaPersona)
@@ -35,12 +37,37 @@ gradoAmbicion unaPersona
 
 -- Punto 2
 -- Punto a: Nombre largo
-nombreLargo (_, _, nombre, _, _) = length nombre > 10
-
+nombrePersona:: Persona -> Nombre
 nombrePersona (_, _, nombre, _, _) = nombre
-nombreLargo unaPersona = length (nombrePersona unaPersona) > 10
 
-nombreLargo = (length . nombrePersona) > 10
-
+nombreLargo :: Persona -> Bool
 nombreLargo = (> 10) . length . nombrePersona
 
+-- Punto b: Persona suertuda
+personaSuertuda :: Persona -> Bool
+personaSuertuda = even . (* 3) . coeficienteSatisfaccion
+
+-- Punto c: Nombre lindo
+personaSuertuda :: Persona -> Bool
+nombreLindo = (== 'a') . last . nombrePersona
+
+-- Punto 3
+agregarFelicidonios (_, _, _, felicidonios, _) cantidad = (_, _, _, felicidonios + cantidad, _)
+
+recibirseDeCarrera :: Persona -> String -> Persona
+recibirseDeCarrera (edad, cantidadSuenios, nombre, felicidonios, habilidades) carrera =
+	(edad, cantidadSuenios, nombre, felicidonios + (length carrera) * 1000, habilidades ++ carrera)
+
+viajarAListaDeCiudades :: Persona -> String -> Persona
+viajarAListaDeCiudades (edad, cantidadSuenios, nombre, felicidonios, habilidades) [ciudades] =
+	(edad + 1, cantidadSuenios, nombre, felicidonios + (length ciudades) * 100, habilidades)
+
+enamorarseDeOtraPersona :: Persona -> Persona -> Persona
+enamorarseDeOtraPersona (edad, cantidadSuenios, nombre, felicidonios, habilidades) otraPersona =
+	 (edad, cantidadSuenios, nombre, felicidonios + felicidoniosPersona otraPersona, habilidades)
+
+queTodoSigaIgual :: Persona -> Persona
+queTodoSigaIgual = id
+
+comboPerfecto :: Persona -> Persona
+comboPerfecto unaPersona = agregarFelicidonios (viajarAListaDeCiudades (recibirseDeCarrera unaPersona "Medicina") ["Berazategui", "Paris"]) 100
