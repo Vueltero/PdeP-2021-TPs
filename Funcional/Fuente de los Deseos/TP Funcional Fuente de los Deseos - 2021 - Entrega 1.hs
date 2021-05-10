156044a5f8a13894aaa4b5868f3f544b186cadb3
Agustina <arazanov@frba.utn.edu.ar>
2021-05-10 01:01:38 -0300

    Update TP Funcional Fuente de los Deseos - 2021 - Entrega 1.hs

--- Funcional/Fuente de los Deseos/TP Funcional Fuente de los Deseos - 2021 - Entrega 1.hs
+++ Funcional/Fuente de los Deseos/TP Funcional Fuente de los Deseos - 2021 - Entrega 1.hs
@@ -39,15 +39,15 @@ melina = ("Melina", 17, 1, 12, ["Levantar una ceja"])
 --Punto 1
 coeficienteDeSatisfaccion :: Persona -> Int
 coeficienteDeSatisfaccion persona
-    | (felicidonios persona) > 100 = (felicidonios persona) * (edad persona)
-    | (felicidonios persona) > 50  = (felicidonios persona) * (suenos persona)
+    | felicidonios persona > 100 = felicidonios persona * edad persona
+    | felicidonios persona > 50  = felicidonios persona * suenos persona
     | otherwise = div (felicidonios persona) 2
 
 gradoDeAmbicion :: Persona -> Int
 gradoDeAmbicion persona
-    | (felicidonios persona) > 100 = (suenos persona) * (felicidonios persona)
-    | (felicidonios persona) > 50  = (suenos persona) * (edad persona)
-    | otherwise = (suenos persona) * 2
+    | felicidonios persona > 100 = suenos persona * felicidonios persona
+    | felicidonios persona > 50  = suenos persona * edad persona
+    | otherwise = suenos persona * 2
 
 --Punto 2
 nombreLargo :: Persona -> Bool
@@ -57,15 +57,15 @@ suertuda :: Persona -> Bool
 suertuda = even . (*3) . coeficienteDeSatisfaccion
 
 nombreLindo :: Persona -> Bool
-nombreLindo = (== 'a') . last . nombre
+nombreLindo = (=='a') . last . nombre
 
 --Funciones auxiliares punto 3
-agregarFelidonicios :: Int -> Persona -> Persona
-agregarFelidonicios numero persona =
+agregarFelicidonios :: Int -> Persona -> Persona
+agregarFelicidonios numero persona =
     (nombre persona,
     edad persona,
     suenos persona,
-    (felicidonios persona) + numero,
+    felicidonios persona + numero,
     habilidades persona)
 
 agregarHabilidad :: String -> Persona -> Persona
@@ -74,32 +74,32 @@ agregarHabilidad habilidad persona =
     edad persona,
     suenos persona,
     felicidonios persona,
-    habilidad : (habilidades persona))
+    habilidad : habilidades persona)
 
 envejecer :: Persona -> Persona
 envejecer persona =
     (nombre persona,
-    (edad persona) + 1,
+    edad persona + 1,
     suenos persona,
     felicidonios persona,
     habilidades persona)
 
 --Punto 3
 recibirseDeUnaCarrera :: String -> Persona -> Persona
-recibirseDeUnaCarrera carrera = (agregarHabilidad carrera)
-    . (agregarFelidonicios . (*1000) . length) carrera
+recibirseDeUnaCarrera carrera = agregarHabilidad carrera
+    . agregarFelicidonios (length carrera * 1000)
 
 viajar :: [String] -> Persona -> Persona
 viajar ciudades = envejecer
-    . (agregarFelidonicios . (*100) . length) ciudades
+    . agregarFelicidonios (length ciudades * 100)
 
 enamorarse :: Persona -> Persona -> Persona
-enamorarse persona1 persona2 = agregarFelidonicios (felicidonios persona2) persona1
+enamorarse = flip (agregarFelicidonios . felicidonios)
 
 queTodoSigaIgual :: Persona -> Persona
 queTodoSigaIgual = id
 
 comboPerfecto :: Persona -> Persona
-comboPerfecto = (recibirseDeUnaCarrera "Medicina")
-    . (viajar ["Berazategui", "París"])
-    . (agregarFelidonicios 100)
+comboPerfecto = recibirseDeUnaCarrera "Medicina"
+    . viajar ["Berazategui", "París"]
+    . agregarFelicidonios 100
